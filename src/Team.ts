d4c91d2fd5c00b5e5d8faf26d9aebcfc6a742b45
Martina Kay Basquez <martinabasquez@gmail.com>
2024-09-06 02:20:39 -0500

    suspiciously uncomplicated.....

--- src/Team.ts
+++ src/Team.ts
@@ -10,4 +10,19 @@ export class Team{
     addPlayer(newPlaya: Player):void{
         this.players.push(newPlaya);
     }
+    removePlayer(removePlaya: Player.players):void{
+        this.players.splice(removePlaya, 1);
+    }
+    setActive(playa: Player.players, benched: boolean):void{
+        this.players[playa].active = benched;
+    }
+    getPlayerCount():number{
+        return this.players.length;
+    }
+    logActivePlayers():number{
+        
+    }
+    getActivePlayers():Player[]{
+        return this.players.filter((p, i) => p.active);
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export default Player;
 
+// !!!! i will be honest -- i did not test any of the methods -- sowie in advance !!!!
+
 interface Player{
     name: string;
     jersey: number;
